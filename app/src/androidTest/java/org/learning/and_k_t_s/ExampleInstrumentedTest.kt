2c13f3d12781bc344946cce5003bb580ba8a87b9
sv053 <mirarlejos12@gmail.com>
2021-09-15 08:25:44 +0300

    empty proj

---
+++ app/src/androidTest/java/org/learning/and_k_t_s/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package org.learning.and_k_t_s
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("org.learning.and_k_t_s", appContext.packageName)
+    }
+}
