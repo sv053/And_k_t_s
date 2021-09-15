1d6fd6796be490ebe4b3ae4952e30cbe3567604a
sv053 <mirarlejos12@gmail.com>
2021-09-15 08:51:17 +0300

    code

--- app/src/main/java/org/learning/and_k_t_s/MainActivity.kt
+++ app/src/main/java/org/learning/and_k_t_s/MainActivity.kt
@@ -2,12 +2,18 @@ package org.learning.and_k_t_s
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
