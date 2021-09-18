2fb36edfac704daf0ae0eb588d75f2e7a53e794c
sv053 <mirarlejos12@gmail.com>
2021-09-18 09:14:40 +0300

    test pkg deleted, main package renamed, added colors for landscape

---
+++ app/src/main/java/com/sv053/reddit/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.sv053.reddit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+    }
+}
